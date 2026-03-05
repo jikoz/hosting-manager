eb1ec4c58f156519cfa76b97609232784440a298
jikoz <rajshekhar5894@gmail.com>
2026-03-05 16:47:12 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -32,18 +32,22 @@ echo -e "${CYAN}3${RESET} ➤ Install Tailscale+UP"
 echo -e "${CYAN}4${RESET} ➤ Install HvmPanel"
 echo -e "${CYAN}5${RESET} ➤ Install Cloudflare"
 echo -e "${CYAN}6${RESET} ➤ Install Rdp"
-echo -e "${CYAN}7${RESET} ➤ Exit"
+echo -e "${CYAN}7${RESET} ➤ One Click Playit Installer"
+echo -e "${CYAN}8${RESET} ➤ Install Localtonet"
+echo -e "${CYAN}9${RESET} ➤ Exit"
 echo ""
-read -p "Select option [1-7] ➜ " opt
+read -p "Select option [1-9] ➜ " opt
 
 case $opt in
-  1) bash <(curl -s https://raw.githubusercontent.com/jikoz/codes/refs/heads/main/script) ;;    
-  2) bash <(curl -s https://raw.githubusercontent.com/jikoz/codes/refs/heads/main/wings) ;;
-  3) curl -fsSL https://tailscale.com/install.sh | sh && sudo tailscale up ;;
-  4) bash <(curl -s https://raw.githubusercontent.com/jikoz/codes/refs/heads/main/hvm.sh) ;;
-  5) bash <(curl -s https://raw.githubusercontent.com/jikoz/codes/refs/heads/main/cloudflare.sh) ;;
-  6) bash <(curl -s https://raw.githubusercontent.com/jikoz/codes/refs/heads/main/rdp.sh) ;;
-  7) exit 0 ;;
+  1) clear && bash <(curl -s https://raw.githubusercontent.com/jikoz/codes/refs/heads/main/script) ;;    
+  2) clear && bash <(curl -s https://raw.githubusercontent.com/jikoz/codes/refs/heads/main/wings) ;;
+  3) clear && curl -fsSL https://tailscale.com/install.sh | sh && sudo tailscale up ;;
+  4) clear && bash <(curl -s https://raw.githubusercontent.com/jikoz/codes/refs/heads/main/hvm.sh) ;;
+  5) clear && bash <(curl -s https://raw.githubusercontent.com/jikoz/codes/refs/heads/main/cloudflare.sh) ;;
+  6) clear && bash <(curl -s https://raw.githubusercontent.com/jikoz/codes/refs/heads/main/rdp.sh) ;;
+  7) clear && bash <(curl -s https://raw.githubusercontent.com/jikoz/codes/refs/heads/main/playit.sh) ;;
+  8) clear && bash <(curl -s https://raw.githubusercontent.com/jikoz/codes/refs/heads/main/localtonet.sh) ;;
+  9) exit 0 ;;
   *) echo "❌ Invalid Option"
      sleep 1
     ;;
